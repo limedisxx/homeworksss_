fa136f79f4976c9410e7f99c0b845704759c352a
kkkkkkkkkk <168189246+limedisxx@users.noreply.github.com>
2024-04-26 19:30:38 +0300

    Динамическая типизация ДЗ

---
+++ dynamic_tipizaciya_homewrk_.py
@@ -0,0 +1,18 @@
+name = 'Александр'
+print("Name:", name)
+
+
+age = 20
+print("Age:", age)
+
+
+new_age = age + 1
+print("New Age:", new_age)
+
+
+is_student = True
+print("Is Student:", is_student)
+
+
+
+print("Name:", name, "Age:", age, "New Age:", new_age, "Is Student:", is_student)
