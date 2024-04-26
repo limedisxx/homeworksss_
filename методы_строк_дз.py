6ddd04f110fdfd7024ee86bc6d90b2c30f4f0a96
kkkkkkkkkk <168189246+limedisxx@users.noreply.github.com>
2024-04-26 21:56:40 +0300

    Организация программ и методы строк

---
+++ методы_строк_дз.py
@@ -0,0 +1,9 @@
+my_string = input("VScode ")
+print(len(my_string))
+
+
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ", ""))
+print(my_string[0])
+print(my_string[-1])
