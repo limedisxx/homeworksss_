bf1860782406fc2d964333377ba7932f43b5a483
kkkkkkkkkk <168189246+limedisxx@users.noreply.github.com>
2024-04-30 19:45:35 +0300

    Цикл 'for'

---
+++ цикл_for_дз.py
@@ -0,0 +1,9 @@
+# д/з
+
+cars_count = ['BMW', 'MB', 'LADA', 'KIA', 'HONDA']
+cars_count_ = 0
+
+for i in range(len(cars_count)):
+    cars_count_ += 10
+
+print('Я езжу на автомобиле марки ' + cars_count)
