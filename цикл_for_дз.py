650557b7923464e4dc6faad7eee4bcbe998860d1
kkkkkkkkkk <168189246+limedisxx@users.noreply.github.com>
2024-05-02 18:45:23 +0300

    Исправление

--- цикл_for_дз.py
+++ цикл_for_дз.py
@@ -1,9 +1,13 @@
 # д/з
 
 cars_count = ['BMW', 'MB', 'LADA', 'KIA', 'HONDA']
+for i in cars_count:
+    print('Я езжу на автомобиле марки ' + i)
+
 cars_count_ = 0
 
 for i in range(len(cars_count)):
     cars_count_ += 10
+    string = str(cars_count_)
 
-print('Я езжу на автомобиле марки ' + cars_count)
+print(string)
