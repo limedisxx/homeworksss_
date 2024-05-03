4ab346e18c6dc68efd3b2001b398134b3ed20633
kkkkkkkkkk <168189246+limedisxx@users.noreply.github.com>
2024-05-03 22:40:30 +0300

    функция_с_параметром

---
+++ функция_с_параметром_дз.py
@@ -0,0 +1,16 @@
+# д/з
+
+def print_params(stuff, thing):
+    nothing_1 = [1, 2, 3]
+    x = nothing_1[2] 
+    nothing_2 = 'Wave'
+    y = (nothing_2)
+    print(stuff)
+    print(thing)
+    return x, y
+
+x, y = print_params('stuff', 'thing')    
+print(x, y)
+print(y, x)
+print(y, y)
+print(x, x)
---
+++ функция_с_параметром_дз_2.py
@@ -0,0 +1,9 @@
+# д/з 
+
+def print_params(value):
+    print(value) 
+    print(value)  
+
+
+print_params('Привет')
+print_params('Мир')
