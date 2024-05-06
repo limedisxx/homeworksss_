6ee50323b7d511df202379c139d2e2c8b95eb1e4
kkkkkkkkkk <168189246+limedisxx@users.noreply.github.com>
2024-05-06 13:08:21 +0300

    Add files via upload

--- цикл_for_дз.py
+++ цикл_for_дз.py
@@ -1,12 +1,8 @@
 # д/з
 
 cars_count = ['BMW', 'MB', 'LADA', 'KIA', 'HONDA']
-for i in cars_count:
-    print('Я езжу на автомобиле марки ' + i)
-
 cars_count_ = 0
-
-for j in range(len(cars_count)):
+for i in cars_count:
     cars_count_ += 10
-
-print("Количество автомобилей увеличено на:", cars_count_)
+    print('Я езжу на автомобиле марки ' + i)
+    print("Количество автомобилей увеличено на:", cars_count_)
