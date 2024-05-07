dd0b9c644215ddd02e41ab647b8d4116c38f334c
kkkkkkkkkk <168189246+limedisxx@users.noreply.github.com>
2024-05-07 17:35:05 +0300

    Пространство имен и способы вызова функции

---
+++ пространство_и_функции_дз.py
@@ -0,0 +1,17 @@
+#д/з
+
+def test():
+    a = 1
+    b = 2
+    
+    print(a)
+    print(b)
+
+def test2(x = 1, y = 5, z = 3):
+    print(x)
+    print(y)
+    print(z)
+
+test()
+
+test2(x = 2, y = 14, z = 521)
